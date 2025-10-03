6c72c71462513c045738958f08c822640c3597f3
jeffersonbraga90 <jeffersonbraga90@gmail.com>
2025-10-03 09:52:03 -0300

    :boom: fix - Ajuste imagens desktop

--- js/script.js
+++ js/script.js
@@ -1,20 +1,39 @@
-// Menu Mobile
-/* const toggle = document.getElementById("menu-toggle");
-const nav = document.getElementById("nav");
-
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  if (nav.classList.contains("active")) {
-    nav.style.display = "block";
-  } else {
-    nav.style.display = "none";
-  }
-}); */
-
 const menuToggle = document.getElementById("menu-toggle");
 const nav = document.getElementById("nav");
+const menuClose = document.getElementById("menu-close");
+
+
+document.querySelectorAll('.nav ul li a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    const href = link.getAttribute('href');
+    if (href && href.startsWith('#')) {
+      e.preventDefault();
+      const section = document.querySelector(href);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+       
+        const nav = document.getElementById('nav');
+        const menuToggle = document.getElementById('menu-toggle');
+        const menuClose = document.getElementById('menu-close');
+        if (window.innerWidth < 768 && nav.classList.contains('active')) {
+          nav.classList.remove('active');
+          nav.style.display = 'none';
+          if (menuToggle) {
+            menuToggle.style.display = 'block';
+            menuToggle.classList.remove('active');
+          }
+          if (menuClose) menuClose.style.display = 'none';
+        }
+      }
+    }
+  });
+});
+
 
 menuToggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  menuToggle.classList.toggle("active");
+  nav.classList.add("active");
+  nav.style.display = "block";
+  menuToggle.classList.add("active");
+  if (menuClose) menuClose.style.display = "block";
+  menuToggle.style.display = "none";
 });
